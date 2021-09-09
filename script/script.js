bbc034d0f68757fae966066b1f7e1e35fd61d406
Giorgio Garofalo <giogar2705@gmail.com>
2021-09-09 16:13:27 +0200

    Add animations

--- script/script.js
+++ script/script.js
@@ -1,3 +1,6 @@
+let messagesLength = 0;
+let messageIndex = 0;
+
 class ChatBubble extends HTMLElement {
     constructor() {
         super();
@@ -5,6 +8,7 @@ class ChatBubble extends HTMLElement {
         // in/out
         const type = this.getAttribute('type');
         this.className = 'bubble bubble-' + type;
+        this.style.display = 'none'
 
         const message = document.createElement('div');
         message.className = 'msg msg-' + type;
@@ -27,7 +31,24 @@ class ChatBubble extends HTMLElement {
         } else {
             this.append(tail, message);
         }
+
+        messagesLength++;
+    }
+}
+
+function animateBubble() {
+    if(messageIndex < messagesLength) {
+        const animate = 'animate__animated animate__fadeInUp'
+        const bubble = document.getElementsByTagName('msg-bubble')[messageIndex];
+        bubble.style.display = ''
+        bubble.className += ' ' + animate;
+        messageIndex++;
     }
 }
 
 customElements.define('msg-bubble', ChatBubble);
+
+setInterval(() => {
+    animateBubble()
+}, 1800);
+animateBubble()
